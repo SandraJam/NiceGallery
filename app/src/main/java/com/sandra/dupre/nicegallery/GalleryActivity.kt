3fcaa5f37bfba11310fb4dd8345ca16319e2958d
SandraJam <sandra.duprep@gmail.com>
2018-08-29 19:50:32 +0200

    Initialize Project

---
+++ app/src/main/java/com/sandra/dupre/nicegallery/GalleryActivity.kt
@@ -0,0 +1,12 @@
+package com.sandra.dupre.nicegallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GalleryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
